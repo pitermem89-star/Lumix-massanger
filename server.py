85e68e9b1ef2ab26e760b3ec982f5f65383fd31e
pitermem89-star <pitermem89@gmail.com>
2026-06-30 18:11:50 +0400

    Enable unsafe Werkzeug for socketio server

--- server.py
+++ server.py
@@ -101,4 +101,4 @@ def disconnect():
 
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=10000)
+    socketio.run(app, host="0.0.0.0", port=10000, allow_unsafe_werkzeug=True)
